e172ced8bee3462b48aa79e52fe2cd0643f4a4b3
SanchitGeez <sanchit3546@gmail.com>
2023-03-19 12:10:19 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 
 function Navbar() {
-    return <div>
-        <img src="D:\AlphaX\Projects\kund-kund\public\images\LOGO.png" alt="logohere"></img>
+    return <div >
+        <div className="navbarbg">
+            <img src="https://kundkund.com/wp-content/uploads/2021/05/1.png" alt="logohere" className="logo"></img>
+            <div className="navbarmenubuttons">
+                <p className="navbarmenu"> COURSES</p>
+                <p className="navbarmenu"> FEATURES</p>
+                <p className="navbarmenu"> BLOG</p>
+                <p className="navbarmenu"> GALERY</p>
+                <p className="navbarlogin"> LOGIN</p>
+            </div>
+            
+        </div>
+        
+        <div className="yellow-line"></div>
     </div>
 }
 
