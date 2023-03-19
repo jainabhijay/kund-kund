dccefad0457f85637684f2bfebe0b081d116d830
SanchitGeez <sanchit3546@gmail.com>
2023-03-19 17:21:59 +0530

    complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
         <div className="navbarbg">
             <img src="https://kundkund.com/wp-content/uploads/2021/05/1.png" alt="logohere" className="logo"></img>
             <div className="navbarmenubuttons">
-                <p className="navbarmenu"> COURSES</p>
-                <p className="navbarmenu"> FEATURES</p>
-                <p className="navbarmenu"> BLOG</p>
-                <p className="navbarmenu"> GALERY</p>
-                <p className="navbarlogin"> LOGIN</p>
+                <button className="navbarmenu"> COURSES</button>
+                <button className="navbarmenu"> FEATURES</button>
+                <button className="navbarmenu"> BLOG</button>
+                <button className="navbarmenu"> GALERY</button>
+                <button className="navbarlogin"> LOGIN</button>
             </div>
             
         </div>
---
+++ src/components/Slides.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Slides(){
+    return <div>
+    </div>
+}
+
+export default Slides;
