c7b817645fe8a64b88c4ed82e66a921ec5924551
SanchitGeez <sanchit3546@gmail.com>
2023-03-20 14:34:12 +0530

    .

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -3,7 +3,12 @@ import Searchbutton from '../images/Searchbutton.png';
 
 function Searchbar(){
     return <div className="searchbg">
-        <p  className="searchtxt">SEARCH</p>
+        {/* <p  className="searchtxt underline-offset-1">SEARCH</p>*/}
+
+        {/* <form action="submit.php" method="post">
+  <input type="image" src="submit-button.png" alt="Submit">
+</form> */}
+        <input type="text" placeholder="Search" className="searchbar"/>
         <img src={Searchbutton} alt="searchicon" className="searchbutton"></img>
     </div>
 }
